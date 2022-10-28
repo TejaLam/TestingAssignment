16ae0087f6888222d0859f81a48cc65377d7a031
Teja Lam <tlam23@sundevils.asu.edu>
2022-10-27 22:15:41 -0700

    success scenario for Count Urinals

--- UrinalsTest/src/Implementation.java
+++ UrinalsTest/src/Implementation.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Implementation
 {
@@ -25,10 +25,57 @@ public class Implementation
     }
 
     public void openFile() {
+        
     }
 
-    public Integer countUrinals(String str) {
-        System.out.println("Not Yet Implemented");
-        return 0;
+    public Integer countUrinals(String input_str) {
+
+
+        boolean isStringGood = goodString(input_str);
+        if(!isStringGood)
+            return -1;
+
+        int total=0;
+        String[] s=input_str.split("");
+        int size=s.length;
+
+        int input_array[]=new int[size];
+
+        int index_1=0;
+        while(index_1<size)
+        {
+            input_array[index_1]=Integer.parseInt(String.valueOf(s[index_1]));
+            index_1++;
+        }
+
+        if(size==1){
+            if(input_array[0]==0){
+                total=1;
+                input_array[0]=1;
+            }
+        }
+
+
+        else {
+            int index_2=0;
+            if(input_array[index_2]==0 && input_array[index_2+1]!=1){
+                input_array[index_2]=1;
+                total++;
+            }
+
+
+        for(index_2=1;index_2<size-1;index_2++){
+            if(input_array[index_2]==0 && input_array[index_2-1]!=1 && input_array[index_2+1]!=1){
+                input_array[index_2]=1;
+                total++;
+            }
+        }
+        if(input_array[index_2]==0&&input_array[index_2-1]!=1) {
+            total++;
+            input_array[index_2] = 1;
+        }
     }
+        return total;
+
+   }
 }
