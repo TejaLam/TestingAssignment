6c3ad4b87a0deb9bd32cc8ada3cf1f70b3319d9c
Teja Lam <tlam23@sundevils.asu.edu>
2022-10-27 19:37:29 -0700

    Tested successfully JUnit with my name and committing

--- UrinalsTest/src/Implementation.java
+++ UrinalsTest/src/Implementation.java
@@ -2,26 +2,32 @@ import java.util.Scanner;
 
 public class Implementation
 {
-
-
     public String getString()
     {
         return null;
     }
 
-    public Boolean goodString(String s)
-    {
-        System.out.println("Not Yet Implemented");
+    public Boolean goodString(String str_input) {
+        for(int index=0;index<str_input.length();index++)
+        {
+            if(str_input.length() > index+1)
+            {
+                if(str_input.charAt(index+1)=='1' && str_input.charAt(index)=='1') {
+                    return false;
+                }
+            }
+            if(str_input.charAt(index)!='1' && str_input.charAt(index)!='0')
+            {
+                return false;
+            }
+        }
         return true;
     }
 
-    public void openFile()
-    {
-
+    public void openFile() {
     }
 
-    public Integer countUrinals(String s)
-    {
+    public Integer countUrinals(String str) {
         return 0;
     }
 }
